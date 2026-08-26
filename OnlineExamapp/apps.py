b5fcd44dce587e10c979385524fc52024c4389c4
Saniya-S19 <ssaniya1902@gmail.com>
2026-08-26 19:16:45 +0530

    Implement user signup and login

--- OnlineExamapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OnlineexamappConfig(AppConfig):
-    name = 'OnlineExamapp'
---
+++ accounts/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password != confirm_password:
+            messages.error(request, 'Passwords do not match!')
+            return redirect('signup')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists!')
+            return redirect('signup')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Email already exists!')
+            return redirect('signup')
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        messages.success(request, 'Account created successfully!')
+        return redirect('user_login')
+    return render(request, 'signup.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            #return redirect('/')
+            messages.success(request, 'Login Successful!')
+
+        messages.error(request, "Invalid username or password.")
+
+    return render(request, 'login.html')
+
+
+# Create your views here.
