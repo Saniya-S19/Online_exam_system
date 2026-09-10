2a62e986ccb95a3fd41cadfc2939c31926576024
Saniya-S19 <ssaniya1902@gmail.com>
2026-09-10 22:42:00 +0530

    Implemented student dashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('signup/',views.signup, name='signup'),
     path('login/',views.user_login, name='login'),
+    path('dashboard/',views.dashboard, name='dashboard'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Course
 
 def home(request):
     return render(request, 'home.html')
@@ -44,11 +46,18 @@ def user_login(request):
         if user is not None:
             login(request, user)
             messages.success(request, 'Login Successful!')
-            return redirect('/')
+            return redirect('dashboard')
 
         messages.error(request, "Invalid username or password.")
         return redirect('user_login')
 
     return render(request, 'login.html')
+
+@login_required(login_url='login')
+def dashboard(request):
+    courses = Course.objects.all()
+    context = {'courses': courses}
+    return render(request, 'dashboard.html', context)
+
 
 
